53f05829ccb8cee3699a6b5026468fdf3b85a780
ico12319 <hristokubev@gmail.com>
2024-05-13 15:27:15 +0300

    update

---
+++ 1.Stacks and Queues/1. Reverse String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+
+        var reversed = ReverseString(input);
+
+        Console.WriteLine(reversed);
+        Console.ReadKey();
+    }
+
+
+    static string ReverseString(string input)
+    {
+        var stack = new Stack<char>();
+
+       for(int i = 0; i < input.Length; i++)
+       {
+            stack.Push(input[i]);
+       }
+
+        var res = string.Empty;
+        foreach (var item in stack)
+        {
+            res += item;
+        }
+
+        return res;
+    }
+
+}
