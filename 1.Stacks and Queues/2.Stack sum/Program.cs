c86f641328440ac51ecfa40d7dd7f2f923d96f06
ico12319 <hristokubev@gmail.com>
2024-05-13 16:32:01 +0300

    update tasks

---
+++ 1.Stacks and Queues/2.Stack sum/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+        fillStack(stack);
+
+
+        while (true)
+        {
+            var input = Console.ReadLine().Split(' ').ToList();
+
+            var main = input[0];
+
+            if(main == "end")
+            {
+                break;
+            }
+
+            else if(main == "Add")
+            {
+                int num1 = int.Parse(input[1]);
+                int num2 = int.Parse(input[2]);
+                addNums(stack, num1, num2);
+            }
+            else if(main == "Remove")
+            {
+                int count = int.Parse(input[1]);
+                if(stack.Count > count)
+                {
+                    removeElements(stack, count);
+                }
+            }
+        }
+
+        Console.WriteLine("Sum: {0}",findSum(stack));
+        Console.ReadKey();
+
+    }
+    
+
+    static void addNums(Stack<int> stack,int n1,int n2)
+    {
+        stack.Push(n1);
+        stack.Push(n2);
+    }
+
+    static void removeElements(Stack<int> stack,int elToDel)
+    {
+        while (elToDel != 0)
+        {
+            stack.Pop();
+            elToDel--;
+        }
+    }
+
+    static int findSum(Stack<int> stack)
+    {
+        var sum = 0;
+        foreach (var item in stack)
+        {
+            sum += item;
+        }
+
+        return sum; 
+    }
+
+    static void print(Stack<int> stack)
+    {
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static void fillStack(Stack<int> stack)
+    {
+        var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            stack.Push(numbers[i]);
+        }
+    }
+}
---
+++ 1.Stacks and Queues/3.Simple Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stack = new Stack<string>();
+        var input = Console.ReadLine().Split().ToList();
+        input.Reverse();
+
+        fillStack(stack, input);
+        printStack(stack);
+
+        Console.WriteLine(manipulateStack(stack));
+        Console.ReadKey();
+    }
+
+    static void fillStack(Stack<string> stack, List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            stack.Push(list[i].ToString());
+        }
+    }
+
+    static void printStack(Stack<string> stack)
+    {
+        Console.WriteLine(string.Join(" ", stack));
+    }
+
+    static int manipulateStack(Stack<string> stack)
+    {
+        var res = int.Parse(stack.Pop());
+
+        while (stack.Count > 0)
+        {
+            var sign = stack.Pop();
+            var number = int.Parse(stack.Pop());
+
+            if (sign == "+")
+            {
+                res += number;
+            }
+            else
+            {
+                res -= number;
+            }
+        }
+
+        return res;
+    }
+
+}
