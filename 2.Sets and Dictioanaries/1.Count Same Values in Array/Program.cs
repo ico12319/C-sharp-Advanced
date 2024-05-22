ddcb7818337da32d3a627610f7d92bbd7700ddcf
ico12319 <hristokubev@gmail.com>
2024-05-22 21:18:50 +0300

    add solutions

---
+++ 2.Sets and Dictioanaries/1.Count Same Values in Array/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+var numbersByCount = new Dictionary<double, int>();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (!numbersByCount.ContainsKey(numbers[i]))
+    {
+        numbersByCount.Add(numbers[i], 0);
+    }
+    numbersByCount[numbers[i]]++;
+}
+
+foreach (var item in numbersByCount)
+{
+    Console.WriteLine($"{item.Key} - {item.Value} times");
+}
+
+Console.ReadKey();
